fa2a2d45b4be6022b8dba93d9fea15dcd86902f1
Vinny-vy <vinnychopra1999@gmail.com>
2019-12-09 12:04:50 +0530

    Updates

---
+++ precache-manifest.d78c8e94efb737116cee250a4cddab85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73be202661472953e8d1183e636d615",
+    "url": "/Vinny-vy/test-react.git/index.html"
+  },
+  {
+    "revision": "b6ff3f651d3e116e910a",
+    "url": "/Vinny-vy/test-react.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f2a49d045aa3a0cac43a",
+    "url": "/Vinny-vy/test-react.git/static/js/2.2b1f4b48.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Vinny-vy/test-react.git/static/js/2.2b1f4b48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6ff3f651d3e116e910a",
+    "url": "/Vinny-vy/test-react.git/static/js/main.9651e6f8.chunk.js"
+  },
+  {
+    "revision": "d03e70548bb361df9553",
+    "url": "/Vinny-vy/test-react.git/static/js/runtime-main.32d454df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Vinny-vy/test-react.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
